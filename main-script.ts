97c47178e555236eae663df70859de0bf66fa010
Andrew Ferro <45604776+acing3@users.noreply.github.com>
2019-04-25 10:05:32 -0400

    commit

---
+++ main-script.ts
@@ -0,0 +1,21 @@
+
+// let main = ():void => {
+// let image = document.getElementById("flip-box") as HTMLImageElement;
+// image.onclick = ():void => {
+//     image.classList.add("flipass");
+// };
+// };
+// main();
+// leftArrow.onclick = () => {
+//     box1.style.display = "block";
+//     alert("here");
+// };
+// let main = ():void => {
+// const leftArrow = document.getElementsByClassName("left-Arrow");
+// const box1 = document.getElementById("project-box1") as HTMLDivElement;
+// let handleSubmitClick = ():void => {
+//     box1.style.display = "block";
+//     alert("here");
+// };
+// };
+// main();
